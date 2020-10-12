69ee8e345d4f1971222e113d08beea8d1d19246c
Anton Kim <ki-an-vi@mail.ru>
2020-10-12 09:54:52 +0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using MVCLab5.ActionFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLab5.Controllers
+{
+    [MyLogActionFilter]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "This is ASP.Net MVC Filters Tutorial";
+        }
+        [OutputCache(Duration = 20)]
+        public string GetCurrentTime()
+        {
+            return DateTime.Now.ToString("T");
+        }
+
+    }
+}
